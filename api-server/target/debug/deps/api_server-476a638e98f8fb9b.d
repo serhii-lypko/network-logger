db3a4feb740fc561f6ab35a58aed3371a47885ee
Serhii Lypko <lypko.sg@gmail.com>
2024-03-24 13:28:25 +0000

    Init

---
+++ api-server/target/debug/deps/api_server-476a638e98f8fb9b.d
@@ -0,0 +1,5 @@
+/Users/serhiilk/Projects/network-logger/api-server/target/debug/deps/libapi_server-476a638e98f8fb9b.rmeta: src/main.rs
+
+/Users/serhiilk/Projects/network-logger/api-server/target/debug/deps/api_server-476a638e98f8fb9b.d: src/main.rs
+
+src/main.rs:
---
+++ api-server/target/debug/deps/api_server-c2e7b3241056ba9b.d
@@ -0,0 +1,5 @@
+/Users/serhiilk/Projects/network-logger/api-server/target/debug/deps/libapi_server-c2e7b3241056ba9b.rmeta: src/main.rs
+
+/Users/serhiilk/Projects/network-logger/api-server/target/debug/deps/api_server-c2e7b3241056ba9b.d: src/main.rs
+
+src/main.rs:
