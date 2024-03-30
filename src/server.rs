6f490aad3da8d0021256119be6bf1beb57c04760
Serhii Lypko <lypko.sg@gmail.com>
2024-03-30 19:59:43 +0000

    Basic NetworkLogRecord structure

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+
 use tonic::{transport::Server, Request, Response, Status};
+
+use chrono::Utc;
 
 use packets_transfer::packets_transfer_server::{PacketsTransfer, PacketsTransferServer};
 use packets_transfer::{PacketsData, TransferAck};
@@ -11,9 +15,43 @@ pub mod packets_transfer {
 
 mod dns_parser;
 
+// TODO: it's important to make sure the cloud DB storage won't be exceeded too fast -> write metrics
+
+/* ------------- ------------- ------------- ------------- -------------
+Database (document?) schema
+
+{
+    timstamp: 123456,
+    dns_log: {
+        "google.com": 12,
+        "facebook.com": 3,
+        "api.openai.com": 6
+    }
+}
+
+------------- ------------- ------------- ------------- ------------- */
+
+// TODO: better naming?
+struct NetworkLogRecord {
+    timestamp: i64,
+    network_log: HashMap<String, u32>,
+}
+
+impl From<(i64, Vec<String>)> for NetworkLogRecord {
+    fn from((timestamp, dns_list): (i64, Vec<String>)) -> Self {
+        // TODO
+        let network_log = HashMap::new();
+
+        NetworkLogRecord {
+            timestamp,
+            network_log,
+        }
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct PacketTransferService {
-    foo: String,
+    foo: String, // TODO: database instance instead?
 }
 
 impl PacketTransferService {
@@ -29,13 +67,13 @@ impl PacketsTransfer for PacketTransferService {
         packets_data_request: Request<PacketsData>,
     ) -> Result<Response<TransferAck>, Status> {
         let packets_data = packets_data_request.get_ref().clone();
-        // let packets: Vec<&[u8]> = packets_data_request.split(|&byte| byte == 0x1E).collect();
 
         let timestamp = packets_data.timestamp;
         let dns_records = DNSParser::parse_packets(packets_data.packets);
 
-        dbg!(dns_records);
-        print!("---------------------------");
+        let network_log_record: NetworkLogRecord = (timestamp, dns_records).into();
+
+        // TODO: write network_log_record to DB (mongo?)
 
         let reply = packets_transfer::TransferAck {
             message: "Acknowledge the packet transfer".to_string(),
